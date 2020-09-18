2f9ef5f183367719bb85f16d5bcb46c7819a774e
Keenan <keenanhallas@gmail.com>
2020-09-18 10:36:07 -0500

    register endpoint working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,35 @@
+const bcrypt = require("bcryptjs");
+const { response } = require("express");
+const db = require("../database/dbConfig");
+
 const router = require('express').Router();
+
+router.get("/users", (req, res) => {
+  console.log("getting");
+  db("users")
+    .then(users => {
+      res.status(200).json({ users: users });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "User couldn't be added" });
+    });
+})
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const rounds = process.env.BCRYPT_ROUNDS || 4;
+
+  user.password = bcrypt.hashSync(user.password, rounds);
+
+  db("users")
+    .insert(user)
+    .then(response => {
+      res.status(201).json({ message: "User registered" });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "User couldn't be added" });
+    });
 });
 
 router.post('/login', (req, res) => {
