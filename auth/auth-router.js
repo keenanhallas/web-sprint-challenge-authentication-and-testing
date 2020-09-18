fd3a2d0ccc7b2307c3cb61f81208ba18fc34d78b
Keenan <keenanhallas@gmail.com>
2020-09-18 11:08:50 -0500

    jokes endpoint using the auth middleware & returning jokes when logged in

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -16,12 +16,13 @@ router.get("/users", (req, res) => {
 
 router.post('/register', (req, res) => {
   // implement registration
-  let user = req.body;
-  const rounds = process.env.BCRYPT_ROUNDS || 4;
-
-  user.password = bcrypt.hashSync(user.password, rounds);
-
-  db("users")
+  if(req.body.username && req.body.password){
+    let user = req.body;
+    const rounds = process.env.BCRYPT_ROUNDS || 4;
+    
+    user.password = bcrypt.hashSync(user.password, rounds);
+    
+    db("users")
     .insert(user)
     .then(response => {
       res.status(201).json({ message: "User registered" });
@@ -29,24 +30,30 @@ router.post('/register', (req, res) => {
     .catch(err => {
       res.status(500).json({ message: "User couldn't be added" });
     });
+  } else {
+    res.status(500).json({ message: "Please supply both a username & password" });
+  }
 });
 
 router.post('/login', (req, res) => {
   // implement login
   let { username, password } = req.body;
-
-  db("users")
-    .where("username", "=", username)
-    .first()
-    .then(user => {
-      if(user && bcrypt.compareSync(password, user.password)) {
-        req.session.user = user;
-        res.status(200).json({ Message: `Welcome ${user.username}!` });
-      }
-    })
-    .catch(err => {
-      res.status(401).json({ message: "Invalid credentials" });
-    });
+  if(username && password) {  
+    db("users")
+      .where("username", "=", username)
+      .first()
+      .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+          req.session.user = user;
+          res.status(200).json({ Message: `Welcome ${user.username}!` });
+        }
+      })
+      .catch(err => {
+        res.status(401).json({ message: "Invalid credentials" });
+      });
+  } else {
+      res.status(500).json({ message: "Please supply both a username & password" });
+    }
 });
 
 module.exports = router;
